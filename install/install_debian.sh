adce6a3f8ff00a6502700a8d729213cfa8ca3796
mgr <thomas.bohl@gmail.com>
2016-09-30 01:03:30 +0200

    install script fixed

--- install/install_debian.sh
+++ install/install_debian.sh
@@ -2,13 +2,13 @@
 sudo apt update && sudo apt upgrade -y
 
 # libass / ffmpeg / mpv dependencies
-sudo apt install libfreetype6-dev libfribidi-dev libfontconfig1-dev
+sudo apt install libfreetype6-dev libfribidi-dev libfontconfig1-dev -y
 sudo apt install yasm libx264-dev  -y
 sudo apt install git libtool build-essential pkg-config autoconf -y
 sudo apt install liblua5.1-0-dev libluajit-5.1-dev -y
 sudo apt install libvdpau-dev libva-dev libxv-dev libjpeg-dev libxkbcommon-dev libxinerama-dev -y
-sudo apt install libxrandr-dev libgles2-mesa-dev libgles1-mesa-dev libv4l-dev libxss-dev libgl1-mesa-dev libgl2-mesa-dev -y
-
+sudo apt install libxrandr-dev libgles2-mesa-dev libgles1-mesa-dev libv4l-dev libxss-dev libgl1-mesa-dev -y
+sudo apt install libcaca-dev libsdl2-dev libasound2-dev
 # hplayer2 dependencies
 
 
@@ -21,6 +21,6 @@ sudo apt install libxrandr-dev libgles2-mesa-dev libgles1-mesa-dev libv4l-dev li
 git clone https://github.com/mpv-player/mpv-build.git
 cd mpv-build
 echo --enable-libmpv-shared > mpv_options
-echo --enable-mmal >> ffmpeg_options
+echo --enable-mmal > ffmpeg_options
 ./rebuild -j4
 #sudo ./install
