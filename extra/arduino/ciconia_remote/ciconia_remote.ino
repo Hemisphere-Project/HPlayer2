4b922d685f5ed4d3f3f655d6be9709e55b506648
maigre <thomas.bohl@gmail.com>
2018-10-01 15:01:21 +0200

    Ciconia remote

--- extra/arduino/ciconia_remote/ciconia_remote.ino
+++ extra/arduino/ciconia_remote/ciconia_remote.ino
@@ -2,6 +2,7 @@
    SETTINGS
 */
 #define CR_VERSION  0.01  // Init
+#define CR_VERSION  0.02  // Fix on disconnect
 
 /*
    INCLUDES
@@ -21,9 +22,10 @@ SSD1306 display(0x3c, 5, 4); // instance for the OLED. Addr, SDA, SCL
 // TOUCH
 // www.areresearch.net/2018/01/how-to-use-ttgo-esp32-module-with-oled.html
 // https://github.com/espressif/arduino-esp32/issues/112
-int threshold = 40;
-byte doNext = 0;
-byte doStop = 0;
+int threshold = 70;
+bool ready = false;
+long doNext = 0;
+long doStop = 0;
 
 //UDP
 WiFiUDP udp_in;
@@ -31,6 +33,13 @@ WiFiUDP udp_out;
 char udpPacket[1472];
 
 long lastInfo = 0;
+long lastNews = 0;
+
+//BTN
+const byte interruptPin = 22;
+
+//MUTEX
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
 /*
    SETUP
@@ -58,7 +67,9 @@ void setup() {
   touchAttachInterrupt(T2, touch2, threshold);  // Touch 2 = GPIO 2
   touchAttachInterrupt(T8, touch8, threshold);  // Touch 8 = GPIO 33
 
-  LOG(HTTPCLIENT_DEFAULT_TCP_TIMEOUT);
+  // Btn
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), touch2, FALLING);
 
   //UDP
   // input socket
@@ -72,21 +83,26 @@ void loop() {
   if (wifi_isok()) {
 
     //LOG(touchRead(T2));
-
+    ready = true;
+    
+    //LOG("processing commands");
     // Commands
     if (doNext == 1) {
       httpGet("/next");
-      doNext = 2;
+      doNext = millis()+200;
     }
     if (doStop == 1) {
       httpGet("/stop");
-      doStop = 2;
+      doStop = millis()+200;
     }
 
-    if (touchRead(T2) > threshold) doNext = 0;
-    if (touchRead(T8) > threshold) doStop = 0;
+    if (doNext >= 10 && doNext < millis()) doNext = 2;
+    if (doStop >= 10 && doStop < millis()) doStop = 2;
+    
+    if (doNext == 2 && touchRead(T2) > threshold) doNext = 0;
+    if (doStop == 2 && touchRead(T8) > threshold) doStop = 0;
 
-
+    //LOG("receiving info");
     if (udp_in.parsePacket()) {
       //Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
       int len = udp_in.read(udpPacket, 1470);
@@ -95,9 +111,11 @@ void loop() {
         //LOGF("UDP: packet received: %s\n", udpPacket);
         dispStatus( getValue(udpPacket, '/', 0), getValue(udpPacket, '/', 1) );
         lastInfo = millis();
+        lastNews = millis();
       }
     }
-
+    
+    //LOG("sending synctest");
     if (millis()-lastInfo > 200) {
       // STATUS check
       udp_out.beginPacket("3.0.0.1", 4000);
@@ -105,6 +123,11 @@ void loop() {
       msg.send(udp_out);
       udp_out.endPacket();
       lastInfo = millis()-50;
+    }
+
+    //LOG("checking link");
+    if (millis()-lastNews > 1500) {
+      dispStatus(":: no player ::");
     }
     
     // MEDIA check
@@ -117,27 +140,34 @@ void loop() {
     dispStatus(media);*/
     
   }
-  else delay(100);
+  else {
+    dispStatus(":: disconnected ::");
+  }
+  delay(50);
 }
 
 /*
  * TOUCH 2 (GPIO 2)
  */
 void touch2() {
-  if (doNext == 0) {
+  portENTER_CRITICAL(&mux);
+  if (ready && doNext == 0) {
     LOG("TOUCH 2 (gpio 2)");
     doNext = 1;
   }
+  portEXIT_CRITICAL(&mux);
 }
 
 /*
  * TOUCH 8 (GPIO 33)
  */
 void touch8() {
-  if (doStop == 0) {
+  portENTER_CRITICAL(&mux);
+  if (ready && doStop == 0) {
     LOG("TOUCH 8 (gpio 33)");
     doStop = 1;
   }
+  portEXIT_CRITICAL(&mux);
 }
 
 
@@ -155,8 +185,6 @@ void doOnConnect() {
 */
 void doOnDisconnect() {
   dispStatus(":: disconnected ::");
-  //httpGet("/loop");
-  //httpGet("/play/earth.mp4");
 }
 
 /*
