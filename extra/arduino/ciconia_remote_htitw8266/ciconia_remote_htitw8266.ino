ee0b3eeff8f4e77b3eb7ea49159f38f683df1179
maigre <thomas.bohl@gmail.com>
2019-02-07 00:31:00 +0100

    teleco 1watt

--- extra/arduino/ciconia_remote_htitw8266/ciconia_remote_htitw8266.ino
+++ extra/arduino/ciconia_remote_htitw8266/ciconia_remote_htitw8266.ino
@@ -2,19 +2,18 @@
    SETTINGS
 */
 #define CR_VERSION  0.01  // Init
+#define CR_VERSION  0.02  // 1watt teleco
+
+#define SLEEPTIME 5       // minutes before going to sleep
 
 #include <Arduino.h>
 #include <OSCMessage.h> //https://github.com/stahlnow/OSCLib-for-ESP8266
 #include "debug.h"
 #include "wifi.h"
 
+enum events { NEXT, PREV, STOP };
+
 bool shuttingDown = false;
-
-bool ready1 = false;
-long doNext = 0;
-long doPrev = 0;
-long doStop = 0;
-
 
 //UDP
 WiFiUDP udp_in;
@@ -23,10 +22,6 @@ char udpPacket[1472];
 
 long lastInfo = 0;
 long lastNews = 0;
-
-//BTN
-const byte nextPin = 3;   //RX
-const byte prevPin = 13;  //D7
 
 
 void setup(void) {
@@ -47,11 +42,30 @@ void setup(void) {
 
   http_init();
 
-  // Btn
-  pinMode(nextPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(nextPin), next, FALLING);
-  pinMode(prevPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(prevPin), prev, FALLING);
+  // D3 : BLUE
+  pinMode(D3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(D3), [](){ event_trigger(D3, media1); }, FALLING);
+
+  // D7: GREEN
+  pinMode(D7, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(D7), [](){ event_trigger(D7, media2); }, FALLING);
+
+  // D6: YELLOW
+  pinMode(D6, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(D6), [](){ event_trigger(D6, media3); }, FALLING);
+
+  // SCL: RED
+  pinMode(14, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(14), [](){ event_trigger(14, media4); }, FALLING);
+  
+  // SDA: PUSH ENC   ----  OK
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), [](){ event_trigger(2, stop); }, FALLING);
+  
+  // ENCODER
+  encoder_init(3, 1);
+  encoder_inc( next );
+  encoder_dec( prev );
 
   //UDP
   // input socket
@@ -59,35 +73,11 @@ void setup(void) {
 }
 
 void loop(void) {
+  encoder_loop();
+  event_loop();
+  
   if (wifi_isok()) {
     wifi_otaCheck();
-    
-    ready1 = true;
-    
-    // Commands
-    if (doNext == 1) {
-      http_get("/next");
-      doNext = millis()+150;
-      oled_clear2();
-    }
-    if (doPrev == 1) {
-      http_get("/prev");
-      doPrev = millis()+150;
-      oled_clear2();
-    }
-    if (doStop == 1) {
-      http_get("/stop");
-      doStop = millis()+150;
-      oled_clear2();
-    }
-
-    if (doNext >= 10 && doNext < millis()) doNext = 2;
-    if (doPrev >= 10 && doPrev < millis()) doPrev = 2;
-    if (doStop >= 10 && doStop < millis()) doStop = 2;
-    
-    if (doNext == 2) doNext = 0;
-    if (doPrev == 2) doPrev = 0;
-    if (doStop == 2) doStop = 0;
 
     if (udp_in.parsePacket()) {
       //Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
@@ -121,7 +111,7 @@ void loop(void) {
   }
   delay(50);
 
-  if (millis()-lastNews > 120*1000) {
+  if (millis()-lastNews > (SLEEPTIME*60*1000)) {
     shutdown();
   }
   //LOG("loop");
@@ -142,34 +132,43 @@ void doOnDisconnect() {
   if (!shuttingDown) oled_status("-no wifi");
 }
 
-/*
- * BTN NEXT
- */
+void media1() {
+  http_get("/play/1");
+  oled_clear2();
+}
+
+void media2() {
+  http_get("/play/2");
+  oled_clear2();
+}
+
+void media3() {
+  http_get("/play/3");
+  oled_clear2();
+}
+
+void media4() {
+  http_get("/play/4");
+  oled_clear2();
+}
+
 void next() {
-  if (ready1 && doNext == 0) {
-    LOG("NEXT");
-    doNext = 1;
-  }
+  http_get("/next");
+  oled_clear2();
 }
 
-/*
- * BTN PREV
- */
 void prev() {
-  if (ready1 && doPrev == 0) {
-    LOG("PREV");
-    doPrev = 1;
-  }
+  http_get("/prev");
+  oled_clear2();
 }
 
-/*
- * TOUCH 8 (GPIO 33)
- */
 void stop() {
-  if (ready1 && doStop == 0) {
-    LOG("TOUCH 8 (gpio 33)");
-    doStop = 1;
-  }
+  http_get("/stop");
+  oled_clear2();
+}
+
+void test() {
+  oled_status("test");
 }
 
 void shutdown() {
@@ -178,6 +177,5 @@ void shutdown() {
   oled_status("          ","          ");
   WiFi.mode(WIFI_OFF);
   ESP.wdtDisable();
-  //ESP.deepSleep(999999999*999999999U, WAKE_NO_RFCAL);
   ESP.deepSleep(0);
 }
