a2fa8e77def9f28a12e0273578cf4d096a5b8e4e
maigre <thomas.bohl@gmail.com>
2019-05-23 22:08:03 +0200

    remote hidesee

---
+++ extra/arduino/hidesee_remote_htitw8266/debug.h
@@ -0,0 +1,19 @@
+//#define DEBUG
+
+#ifdef DEBUG
+ #define LOGSETUP()                    Serial.begin(115200)
+ #define LOGINL(x)                     Serial.print (x)
+ #define LOGDEC(x)                     Serial.print (x, DEC)
+ #define LOGF(x, y)                    Serial.printf (x, y)
+ #define LOGF2(x, y1, y2)              Serial.printf (x, y1, y2)
+ #define LOGF4(x, y1, y2, y3, y4)      Serial.printf (x, y1, y2, y3, y4)
+ #define LOG(x)                        Serial.println (x)
+#else
+ #define LOGSETUP()     
+ #define LOGINL(x)   
+ #define LOGDEC(x)
+ #define LOGF(x, y)
+ #define LOGF2(x, y1, y2)
+ #define LOGF4(x, y1, y2, y3, y4)              
+ #define LOG(x)         
+#endif
