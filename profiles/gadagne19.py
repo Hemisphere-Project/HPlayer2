1d2f4d3d1ec8cbcba6e1a331d9ddb4a49ce363ee
maigre <thomas.bohl@gmail.com>
2019-10-21 13:46:23 +0000

    gadagne19

--- profiles/gadagne19.py
+++ profiles/gadagne19.py
@@ -7,7 +7,7 @@ player.doLog['cmds'] = True
 
 # Interfaces
 player.addInterface('http', 8080)
-player.addInterface('http2', 8088)
+player.addInterface('http2', 80)
 if hplayer.isRPi():
     player.addInterface('gpio', [20,21,16])
 
@@ -18,14 +18,14 @@ player.unbind('end-playlist', player.stop)
 player.on(['player-ready', 'end-playlist'], lambda: player.play("0_*.*"))
 
 # HTTP + GPIO events
-player.on(['push1', 'gpio21'], lambda: player.play("1_*.*"))
-player.on(['push2', 'gpio20'], lambda: player.play("2_*.*"))
-player.on(['push3', 'gpio16'], lambda: player.play("3_*.*"))
+player.on(['push1', 'gpio21-on'], lambda: player.play("1_*.*"))
+player.on(['push2', 'gpio20-on'], lambda: player.play("2_*.*"))
+player.on(['push3', 'gpio16-on'], lambda: player.play("3_*.*"))
 
 
 # PATH
-hplayer.setBasePath("/home/mgr/Videos/test")
-hplayer.persistentSettings("/home/mgr/Videos/gadagne/hplayer2-gadagne19.cfg")
+hplayer.setBasePath("/data/media")
+hplayer.persistentSettings("/data/hplayer2-gadagne19.cfg")
 
 # DISABLE automations
 player.loop(False)
