278b897a75375064b8f6cdb42941314a89a3173a
maigre <thomas.bohl@gmail.com>
2019-11-04 13:55:43 +0100

    build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -122,20 +122,29 @@ cd mpv-build
 
 # RPi: enable MMAL
 if [[ $(uname -m) = armv* ]]; then
-	echo --enable-mmal > ffmpeg_options
-	echo --disable-vaapi > mpv_options
-
-	## TODO : disable Vulkan detection in WAF
+	#echo --enable-mmal > ffmpeg_options
+	echo --enable-libv4l2 > ffmpeg_options
+    echo --disable-vaapi > mpv_options
+	echo --enable-rpi > mpv_options
 fi
 
 # Build
 ./use-mpv-release
 ./use-ffmpeg-release
+
+# fixed rebuild
+set -e
+export LC_ALL=C
 ./update
+./clean
+cd mpv 
+git checkout refs/tags/"v0.29.1"    # 0.30.0 release is broken on RPi
+cd ..
+
 if [[ $(uname -m) = armv* ]]; then
-    ./rebuild -j4
+    ./build -j4
 else
-    ./rebuild -j8
+    ./build -j8
 fi
 cd ..
 
