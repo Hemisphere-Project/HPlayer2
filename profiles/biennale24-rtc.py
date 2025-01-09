58ed10798edcd7ffd4b8ac5e2f7d09dba937afaa
maigre <thomas@37m.gr>
2025-01-09 10:44:56 +0100

    biennale24-rtc

--- profiles/biennale24-rtc.py
+++ profiles/biennale24-rtc.py
@@ -1,9 +1,10 @@
 from core.engine.hplayer import HPlayer2
 from core.engine import network
-from datetime import datetime
+from datetime import datetime, timedelta
 from threading import Timer
 import subprocess
 import time
+import os
 
 # EXTRA TMP UPLOAD
 import tempfile
@@ -26,9 +27,23 @@ player.doLog['cmds'] = False
 
 
 # Interfaces
-hplayer.addInterface('http', 8080)
 hplayer.addInterface('http2', 80, {'playlist': False, 'loop': False, 'mute': True})
 
+# Zyre SYNC
+SYNC = False
+SYNC_MASTER = False
+if os.path.isfile('/boot/wifi/eth0-sync-AP.nmconnection') or os.path.isfile('/boot/wifi/eth0-sync-STA.nmconnection'):
+	SYNC = True
+	SYNC_MASTER = os.path.isfile('/boot/wifi/eth0-sync-AP.nmconnection')
+	hplayer.addInterface('zyre', 'eth0')
+
+elif os.path.isfile('/boot/wifi/wlan0-sync-AP.nmconnection') or os.path.isfile('/boot/wifi/wlan0-sync-STA.nmconnection'):
+	SYNC = True
+	SYNC_MASTER = os.path.isfile('/boot/wifi/wlan0-sync-AP.nmconnection')
+	if network.has_interface('wlan0'):
+		hplayer.addInterface('zyre', 'wlan0')
+	elif network.has_interface('wlan1'):
+		hplayer.addInterface('zyre', 'wlan1')
 
 # PLAY action
 debounceLastTime = 0
@@ -46,29 +61,50 @@ def doPlay(media, debounce=0):
 	hplayer.playlist.play(media)
 
 
-currentDay = -1
+currentDate = None
 checkDayTimer = None
 
 # RTC day changed
-def checkDay():
+def checkDay(date=None):
 	global checkDayTimer
-	global currentDay
+	global currentDate
 
 	if checkDayTimer:
 			checkDayTimer.cancel()
 	checkDayTimer = Timer(30, checkDay)
 	checkDayTimer.start()
 
-	try:
-		date = subprocess.run(['hwclock', '--show'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip().split('+')[0]
-		day_of_year = datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f').timetuple().tm_yday #tm_sec
+	try: 
+		localDate = True
+
+		if not date:
+			date = subprocess.run(['hwclock', '--show'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip().split('+')[0]
+			date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f')
+		else:
+			localDate = False
+
+		day_of_year = date.timetuple().tm_yday #tm_sec
+
+		# send current date
+		if SYNC and localDate:
+			hplayer.interface('zyre').node.broadcast('date', (date+timedelta(days=0)).strftime("%Y-%m-%d"))	
+			return False		
+
+		# Check if day changed
+		currentDay = currentDate.timetuple().tm_yday if currentDate else -1
 
 		if currentDay == day_of_year: 
-			# print('Day not changed', currentDay)
+			print('Day not changed', currentDay)
 			return False
 
-		currentDay = day_of_year
-		print('Day changed to', currentDay)
+		elif currentDate and date < currentDate:
+			print('Old clock, ignoring.. ')
+			return False
+		
+		currentDate = date
+		currentDay = currentDate.timetuple().tm_yday if currentDate else -1
+		print('Day changed to', currentDate.strftime("%Y-%m-%d"), currentDay)
+
 		playDay()
 		return True
 
@@ -76,11 +112,26 @@ def checkDay():
 		print('RTC Error', e)
 		return False
 
+dayOffset = -7
+
 # RTC Clock
 def playDay():
-	media = hplayer.files.currentList()
-	doPlay(media[currentDay%len(media)])
+	currentDay = currentDate.timetuple().tm_yday if currentDate else -1
+	if currentDay >= 0:
+		media = hplayer.files.currentList()
+		doPlay(media[ (currentDay+dayOffset) % len(media) ])
 
+# ON Date
+@hplayer.on('zyre.date')
+def extdate(ev, *args):
+	global currentDate
+	try:
+		print("Ext date received")
+		extdate = datetime.strptime(args[0], '%Y-%m-%d')
+		if not currentDate or extdate > currentDate:
+			checkDay(extdate)
+	except Exception as e:
+		print("Ext date error", e)
 
 # DEFAULT File
 @hplayer.on('app-run')
@@ -88,16 +139,17 @@ def playDay():
 @hplayer.on('playlist.end')
 def play0(ev, *args):
 	hplayer.settings.set('loop', 2)
-	if not checkDay(): playDay()
+	# if not checkDay(): playDay()
+	checkDay()
 
 # HTTP2 Play -> disable loop && do propagate
 @hplayer.on('http2.play')
 def play2(ev, *args):
-	global currentDay, checkDayTimer
+	global currentDate, checkDayTimer
 	if checkDayTimer:
 			checkDayTimer.cancel()
 	doPlay(args[0])
-	currentDay = -1
+	currentDate = None
 	checkDayTimer = Timer(5, checkDay)
 	checkDayTimer.start()
 
