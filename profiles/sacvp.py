b2f03fa6a20d6971a850c8c29aaccab4b22ebc4d
maigre <thomas@37m.gr>
2025-03-14 01:21:02 +0000

    disable esp

--- profiles/sacvp.py
+++ profiles/sacvp.py
@@ -204,7 +204,7 @@ def keyboard(ev, *args):
 
 # ESP -> MQTT / BT
 lastEspEvent = 'sacvp.esp'  # save last event
-@hplayer.on('*.esp')
+#@hplayer.on('*.esp')
 def espRelay(ev, *args):
     if myESP:
         global lastEspEvent
@@ -214,7 +214,7 @@ def espRelay(ev, *args):
 
 
 # File name -> Trigger ESP
-@hplayer.on('*.playing')
+#@hplayer.on('*.playing')
 def espPlay(ev, *args):
     last = args[0].split('.')[0].split('_')[-1]
     if last[0] == 'L' and len(last) > 1:
@@ -226,8 +226,8 @@ def espPlay(ev, *args):
 
 
 # Stop -> Blackout ESP
-@hplayer.on('*.stopped')
-@hplayer.on('*.paused')
+#@hplayer.on('*.stopped')
+#@hplayer.on('*.paused')
 def espStop(ev, *args):
     global lastEspEvent
     if lastEspEvent == 'sacvp.esp':
@@ -238,7 +238,7 @@ def espStop(ev, *args):
 @video.on('ready')
 def init(ev, *args):
     hplayer.settings.set('volume', 100)
-    hplayer.settings.set('loop', -1)
+    hplayer.settings.set('loop', 1)
 
 
 # file = hplayer.imgen.txt2img("004F006B00200073007500700065007200202764FE0F", "UCS2")
